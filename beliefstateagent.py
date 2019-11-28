fixed variance of normal dist

beliefstateagent.py
@@ -54,7 +54,7 @@ class BeliefStateAgent(Agent):
         beliefStates = self.beliefGhostStates
 
         # XXX: Your code here
-        print(1-scipy.stats.norm(0, 1).cdf(0))
+        
 
         for i in range(beliefStates):
             beliefStates[i] = sensorModel(evidences[i], beliefStates[i], pacman_position)
@@ -69,7 +69,7 @@ class BeliefStateAgent(Agent):
         for i in range(beliefState.width):
             for j in range(beliefState.height):
                 if self.walls == 0:
-                    beliefState[i][j] = beliefState[i][j] * (1-scipy.stats.norm(0, 1).cdf(abs(manhattanDistance(pacmanPos, (i,j))-noisyDist)))
+                    beliefState[i][j] = beliefState[i][j] * (1-scipy.stats.norm(0, self.sensor_variance).cdf(abs(manhattanDistance(pacmanPos, (i,j))-noisyDist)))
                 else:
                     beliefState[i][j] = 0
         return beliefState